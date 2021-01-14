8571af4875400f5792ee677dff7daf6aaaf2746b
דודי סידון <DUDI@shagrir.co.il>
2021-01-14 12:09:47 +0200

    Experience Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
+import Experience from "./components/Experience";
 
 function App() {
   return (
@@ -34,6 +35,7 @@ function App() {
         <Header/>
         <AboutMe/>
         <Services/>
+        <Experience/>
     </>
   );
 }
