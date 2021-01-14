7e87b13d3889b00ecd6baefdcf8db18912bca1b9
דודי סידון <DUDI@shagrir.co.il>
2021-01-14 13:23:00 +0200

    Pofrolio completed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
 import Experience from "./components/Experience";
-
+import Pofrfolio from "./components/Pofrfolio";
 function App() {
   return (
       <>
@@ -36,6 +36,7 @@ function App() {
         <AboutMe/>
         <Services/>
         <Experience/>
+        <Pofrfolio/>
     </>
   );
 }
